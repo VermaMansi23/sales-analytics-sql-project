b2af8ccd749f395a05d8198a6944eb2b2c5f67d1
VermaMansi23 <47626307+VermaMansi23@users.noreply.github.com>
2025-05-15 22:06:13 +0530

    Update sales_analytics_project.sql

--- sales_analytics_project.sql
+++ sales_analytics_project.sql
@@ -9,28 +9,28 @@ DROP TABLE IF EXISTS Customers;
 
 -- Create tables
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+    customer_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100),
     location VARCHAR(100)
 );
 
 CREATE TABLE Products (
-    product_id INT PRIMARY KEY,
+    product_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     category VARCHAR(100),
-    price DECIMAL(10, 2)
+    price NUMERIC(10, 2)
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
+    order_id SERIAL PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE OrderDetails (
-    order_detail_id INT PRIMARY KEY,
+    order_detail_id SERIAL PRIMARY KEY,
     order_id INT,
     product_id INT,
     quantity INT,
